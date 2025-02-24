7d64fac4553d60b19fa37c8ca2b2f1c591e6718c
jongheonleee <qwefghnm1212@gmail.com>
2025-02-24 12:48:01 +0900

    [refactor] 와일드 카드 사용 제거

--- src/main/kotlin/com/practice/donghang/controller/PostController.kt
+++ src/main/kotlin/com/practice/donghang/controller/PostController.kt
@@ -1,9 +1,20 @@
 package com.practice.donghang.controller
 
-import com.practice.donghang.controller.dto.*
+import com.practice.donghang.controller.dto.PostCreateRequest
+import com.practice.donghang.controller.dto.PostDetailResponse
+import com.practice.donghang.controller.dto.PostSearchRequest
+import com.practice.donghang.controller.dto.PostSummaryResponse
+import com.practice.donghang.controller.dto.PostUpdateRequest
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 @RestController
